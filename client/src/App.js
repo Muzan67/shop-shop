56cfe61fceac8850485fd8bfbc96820eaf8506fd
Alejandro Cortez <102841726+Muzan67@users.noreply.github.com>
2022-09-08 23:16:06 -0400

    finished lesson 4

--- client/src/App.js
+++ client/src/App.js
@@ -14,9 +14,9 @@ import NoMatch from "./pages/NoMatch";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Nav from "./components/Nav";
-import OrderHistory from "./pages/OrderHistory";
-
 import { StoreProvider } from "./utils/GlobalState";
+import Sucess from "./pages/Sucess";
+import OrderHistory from "./pages/OrderHistory";
 
 const httpLink = createHttpLink({
   uri: "/graphql",
@@ -48,6 +48,7 @@ function App() {
               <Route exact path="/" component={Home} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/signup" component={Signup} />
+              <Route exact path="/success" component={Sucess} />
               <Route exact path="/orderHistory" component={OrderHistory} />
               <Route exact path="/products/:id" component={Detail} />
               <Route component={NoMatch} />
